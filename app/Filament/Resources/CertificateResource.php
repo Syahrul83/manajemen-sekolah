b43b1a06c12d8607ec5811826cf2a2d67aece444
Syahrul83 <valefour_syahrul@yahoo.com>
2023-09-08 20:18:58 +0800

    video 16 uplod file in filament amitav roy

---
+++ app/Filament/Resources/CertificateResource.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CertificateResource\Pages;
+use App\Filament\Resources\CertificateResource\RelationManagers;
+use App\Models\Certificate;
+use Filament\Forms;
+use Filament\Forms\Components\FileUpload;
+use Filament\Forms\Components\MarkdownEditor;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Toggle;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Columns\ToggleColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CertificateResource extends Resource
+{
+    protected static ?string $model = Certificate::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name'),
+                MarkdownEditor::make('description'),
+                Toggle::make('is_active')->default(true),
+                FileUpload::make('certificate_image'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('no')->rowIndex(),
+                TextColumn::make('name'),
+                ToggleColumn::make('is_active'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ])
+            ->emptyStateActions([
+                Tables\Actions\CreateAction::make(),
+            ])->defaultSort('id', 'desc');
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCertificates::route('/'),
+            'create' => Pages\CreateCertificate::route('/create'),
+            'edit' => Pages\EditCertificate::route('/{record}/edit'),
+        ];
+    }
+}
